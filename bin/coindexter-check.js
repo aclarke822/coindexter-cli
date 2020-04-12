2d1918259e21741baec30778cc6ff0e4846d517c
Austin Clarke <aclarke822@gmail.com>
2020-04-12 14:52:59 -0500

    update check price | create CryptoAPI class

--- bin/coindexter-check.js
+++ bin/coindexter-check.js
@@ -1,8 +1,11 @@
 const commander = require('commander');
+const check = require('../commands/check')
 
 commander
     .command('price')
     .description('Check price of coins')
-    .action(() => console.log('Hello from price'));
+    .option('--coin <type>', 'Add specific coin types in CSV format', 'BTC,ETH,XRP')
+    .option('--cur <currency>', 'Change the currency', 'USD')
+    .action(cmd => check.price(cmd));
 
 commander.parse(process.argv);
---
+++ commands/check.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+
+const check = {
+    price(cmd) {
+        console.log(cmd.coin, cmd.cur)
+    }
+}
+
+module.exports = check;
