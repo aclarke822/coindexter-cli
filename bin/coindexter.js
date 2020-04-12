7d498b3ee972688d640688ca01068510cb00e40b
Austin Clarke <aclarke822@gmail.com>
2020-04-12 13:34:36 -0500

    setup basic commands

--- bin/coindexter.js
+++ bin/coindexter.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
-const commander = require('commander')
+const commander = require('commander');
+const pkg = require('../package.json');
+
+commander
+    .version(pkg.version)
+    .command('key', "Manage API Key -- https://nomics.com")
+    .parse(process.argv);
+
+console.log('Hello from coindexter');
 
 
-console.log('Hello from coindexter')
+//console.log(process.argv[2]);
